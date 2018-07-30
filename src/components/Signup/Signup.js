7692d88a84c8727ac50b7da422357a9d016578b1
Pramisha C P <pramishacp123@gmail.com>
2018-07-30 16:01:23 +0530

    second commit

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import './Signup.css';
+
+class Signup extends Component {
+
+
+  render() {
+  
+    return (
+      
+      <div>
+
+         SIGNUP
+      
+      </div>
+    );
+  }
+}
+
+export default Signup;
